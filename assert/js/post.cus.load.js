84e9d9155ba86b05fd0ac6850b8630f1f6c01333
thuong.nv <mark.ngo@kohyoung.com>
2023-10-02 17:33:37 +0700

    [add] nav menu

--- assert/js/post.cus.load.js
+++ assert/js/post.cus.load.js
@@ -1,21 +1,55 @@
 $(function () {
-    var data = [
-        {
-            label: 'node1',
-            children: [
-                { label: 'child1' },
-                { label: 'child2' }
-            ]
-        },
-        {
-            label: 'node2',
-            children: [
-                { label: 'child3' }
-            ]
-        }
-    ];
 
-    $('#archives-tree-list').tree({
-        data: data
-    });
+    $.getJSON("../tree-archives.json", function(json_data)
+    {
+        var add_link = function ($root_tag, $name, $href){
+            const str_tag_link = '\
+                                <a class="tree-nav__item-title" href="'+ $href + '"> \
+                                    <i class="icon ion-edit"></i>'+ $name + '\
+                                </a>';
+            var tag_link = $(str_tag_link);
+            $root_tag.append(tag_link);
+            return tag_link;
+        };
+
+        var create_tag_folder = function($root_tag, $name){
+            const str_tag_folder = '\
+                            <details class="tree-nav__item is-expandable">\
+                              <summary class="tree-nav__item-title">' + $name + '</summary>\
+                              <div class="tree-nav__item"></div>\
+                            </details>';
+            var tag_folder = $(str_tag_folder);
+            $root_tag.append(tag_folder);
+            return tag_folder;
+        };
+
+        var add_folder = function($root, $items){
+            $.each($items, function(key, item){
+                if(item.type === "folder"){
+                    var tag_folder_added = create_tag_folder($root, item.name);
+                    if(item.sub !== undefined)
+                    {
+                        add_folder(tag_folder_added, item.sub)
+                    }
+                }else if(item.type === "link"){
+                    var tag_push = $root.closest("div.tree-nav__item");
+                    console.log(tag_push)
+                    add_link(tag_push, item.name, item.href);
+                }
+                else {
+                    alert("type not define");
+                }
+            });
+        };
+
+        var root_nav = $("nav.tree-nav");
+
+        var tree_archives = json_data.archives;
+
+        add_folder(root_nav, tree_archives);
+
+    }).fail(function()
+    {
+        alert("An error has accurred.");
+    })
 })
