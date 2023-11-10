42ce8486509faadf9021904439049b947ebb9f1b
thuong.nv <mark.ngo@kohyoung.com>
2023-11-10 16:57:26 +0700

    [update] UI - Menu

--- assert/js/cus.load.js
+++ assert/js/cus.load.js
@@ -1,20 +1,20 @@
-import { treeview_util, src_loader} from "./util.js"
+import { treeview_util, src_loader } from "./util.js"
 
 $(function () {
 
       /* Define Page type */
       const PageType = {
             Common: 1,
-            Post  : 2,
+            Post: 2,
       }
 
       var tag_page_type = $("head").attr("type-page")
 
-      var page_type = String(tag_page_type).toLowerCase() === "Common".toLowerCase() ? 
+      var page_type = String(tag_page_type).toLowerCase() === "Common".toLowerCase() ?
             PageType.Common : PageType.Post
 
       /* Load header and footer section */
-      if(page_type === PageType.Common){
+      if (page_type === PageType.Common) {
             var page_load = $('[page-content]')
             $.each(page_load, function () {
                   var file = 'views/' + $(this).attr('page-content') + '.html'
@@ -22,28 +22,27 @@ $(function () {
             })
 
             /** load tree view home file */
-            $.getJSON("./post/tree-archives.json", function(json_data)
-            {
+            $.getJSON("./post/tree-archives.json", function (json_data) {
                   var root_nav = $("nav.tree-nav");
                   var tree_archives = json_data.archives;
-      
+
                   treeview_util.create_treeview_from_json(root_nav, tree_archives, 0);
             });
 
             // { media }  : { file name }
             const css_loader = [
-                  { media : 'screen', src : 'bootstrap-combined.min.css'},
+                  { media: 'screen', src: 'bootstrap-combined.min.css' },
             ]
 
             // { type }  : { file name }
             const js_loader = [
-                  { type : 'module', src : 'bootstrap.min.js'},
-                  
+                  { type: 'module', src: 'bootstrap.min.js' },
+
             ]
 
             src_loader.load_css('./assert/css/', css_loader);
             src_loader.load_js('./assert/js/', js_loader);
-   
+
       }
       else {
             var page_load = $('[page-content]')
@@ -53,32 +52,34 @@ $(function () {
                   $(this).load(file)
             })
 
-            mathjax : 'http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=default';
+            mathjax: 'http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=default';
 
             // { media }  : { file name }
             const css_loader = [
-                  { media : 'screen', src : 'bootstrap-combined.min.css'},
-                  { media : 'print' , src : 'dfxprint.css'},
-                  { media : 'screen', src : 'prism.css'},
+                  { media: 'screen', src: 'bootstrap-combined.min.css' },
+                  { media: 'print', src: 'dfxprint.css' },
+                  { media: 'screen', src: 'prism.css' },
             ]
 
             // { type }  : { file name }
             const js_loader = [
-                  { type : 'module' , src : 'bootstrap.min.js'},
-                  { type : 'module' , src : 'md-block.js'},
-                  { type : undefined, src : 'prism.js'},
+                  { type: 'module', src: 'bootstrap.min.js' },
+                  { type: 'module', src: 'md-block.js' },
+                  { type: undefined, src: 'prism.js' },
             ]
 
-            
+
             src_loader.load_css('../../assert/css/', css_loader);
             src_loader.load_js('../../assert/js/', js_loader);
-            
+
 
 
       }
 })
 
-$("md-block").ready(function() {
-      var table_of_contents = $(".table-of-contents").detach();
-      $(".ml-tables").appendTo(table_of_contents)
-  });
+$(document).ready(function () {
+      $(".btn-table-of-contents").click(function () {
+            $(".table-of-contents").css("visibility", ($(".table-of-contents").css("visibility") == "visible") ?
+             "hidden" : "visible" );
+      });
+});
